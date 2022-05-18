468caf7567b5541d55b6b156ea5faf5a134bc369
seet23 <seetmansura@gmail.com>
2022-05-18 23:46:29 +0530

    home page

--- src/app/components/components.component.ts
+++ src/app/components/components.component.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit, Renderer2 } from '@angular/core';
-import { NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
-
+import { Component, OnInit, Renderer2, ViewChild } from '@angular/core';
+import { NgbCarousel, NgbDateStruct, NgbSlideEvent, NgbSlideEventSource } from '@ng-bootstrap/ng-bootstrap';
 @Component({
     selector: 'app-components',
     templateUrl: './components.component.html',
+    styleUrls: ['./components.component.scss'],
     styles: [`
     ngb-progressbar {
         margin-top: 5rem;
@@ -19,6 +19,16 @@ export class ComponentsComponent implements OnInit {
     focus2;
     date: {year: number, month: number};
     model: NgbDateStruct;
+    paused = false;
+    unpauseOnArrow = false;
+    pauseOnIndicator = false;
+    pauseOnHover = true;
+    pauseOnFocus = true;
+    images = [965, 982, 738, 965, 982, 1043, 738].map((n) => `https://picsum.photos/id/${n}/900/500`);
+    bannerText = "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance"
+
+    @ViewChild('carousel', {static : true}) carousel: NgbCarousel;
+
     constructor( private renderer : Renderer2) {}
     isWeekend(date: NgbDateStruct) {
         const d = new Date(date.year, date.month - 1, date.day);
@@ -41,5 +51,24 @@ export class ComponentsComponent implements OnInit {
             });
         }
     }
+
+    togglePaused() {
+        if (this.paused) {
+          this.carousel.cycle();
+        } else {
+          this.carousel.pause();
+        }
+        this.paused = !this.paused;
+      }
+
+    onSlide(slideEvent: NgbSlideEvent) {
+        if (this.unpauseOnArrow && slideEvent.paused &&
+          (slideEvent.source === NgbSlideEventSource.ARROW_LEFT || slideEvent.source === NgbSlideEventSource.ARROW_RIGHT)) {
+          this.togglePaused();
+        }
+        if (this.pauseOnIndicator && !slideEvent.paused && slideEvent.source === NgbSlideEventSource.INDICATOR) {
+          this.togglePaused();
+        }
+      }
 
 }
